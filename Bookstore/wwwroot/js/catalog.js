3a58c804bf142ee20c5eb0683edf9da42eceac84
Hassan Work <hassaanmehmood68@gmail.com>
2024-07-04 22:01:09 +0500

    last minute pushes

--- Bookstore/wwwroot/js/catalog.js
+++ Bookstore/wwwroot/js/catalog.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
     $('.scroll-left').on('click', function () {
         // Find the corresponding slider for this button
         var $slider = $(this).closest('.catalog-slider').find('.catalog-container');
-        $slider.animate({ scrollLeft: '-=250' }, 'slow');
+        $slider.animate({ scrollLeft: '-=550' }, 'slow');
     });
 
     // Click event for scrolling right
     $('.scroll-right').on('click', function () {
         // Find the corresponding slider for this button
         var $slider = $(this).closest('.catalog-slider').find('.catalog-container');
-        $slider.animate({ scrollLeft: '+=250' }, 'slow');
+        $slider.animate({ scrollLeft: '+=550' }, 'slow');
     });
 });
 
